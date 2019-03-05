8754c31bd7257adc74b9f20e0fab4751c8750aa6
Lilia Lobato <42565451+LiliaLobato@users.noreply.github.com>
2019-03-04 22:57:52 -0600

    Conversión con Botones sin funciones

--- mainKarim.c
+++ mainKarim.c
@@ -40,55 +40,92 @@
 #include "MK64F12.h"
 #include "MK64F12.h"
 #include <stdint.h>
-static const uint32_t pc6_sw2 = 0x40; //6
-#define FALSE 0
+#include <GPIO.h>
+#include <PushButton.h>
+#include <RGB.h>
+
 #define ADC_0 0
-#define ADC_1 1
+#define DELAY_ARG 600000
 
 uint16_t ADC_result(uint8_t ADC);
-void delay(uint16_t delay);
-uint32_t sw2_pressed();
-uint32_t sw3_pressed();
 
 void main(void) {
+	/**Variable to capture the input value*/
+	uint32_t input_value_SW3 = 0;
+	uint32_t input_value_SW2 = 0;
+
+	//Valor de para el cambio de amplitud
+	//volatile uint16_t amplitud = 0;
+	//Valor para cambio de convolución y no convolución
+	//uint8_t convolucion_flag = 0;
+
+	//Configuración de PushButtons
+	PushButton_sw3_config();
+	PushButton_sw2_config();
+
+	//Variable para guardar la lectura del ADC
 	volatile uint16_t AudIn;
 	uint32_t Salida;
 	uint8_t SalidaL;
 	uint8_t SalidaH;
 
-	SIM->SCGC6 |= SIM_SCGC6_ADC0_MASK; //habilitar reloj a ADC
-	SIM->SCGC5 |= 0xA00; //habilitar reloj a botones
-	SIM->SCGC2 |= 0x1000; //habilitar reloj DAC
+	//Configuración de clock;
+	SIM->SCGC6 |= SIM_SCGC6_ADC0_MASK;
+	SIM->SCGC2 = 0x1000;
 
-	// Configuracion del clock del ADC1
-	SIM->SCGC3 |= SIM_SCGC3_ADC1_MASK;
+	//Configuración de RGB
+	//RGB_green_config();
 
-	PORTC->PCR[6] = 0x100 | 0x03; //habilitar resistencia pull up botón SW2
-	//	PORTA->PCR[4] = 0x100 | 0x03; //habilitar resistencia pull up botón
-
-	GPIOC->PDDR &= ~0x40; //habilitar botón como entrada SW2
-	//	GPIOA->PDDR &= ~0x10; //habilitar botón como entrada
-
-	//	Inicialización del ADC0
-	ADC0->CFG1 = ADC_CFG1_ADIV(
-			0)|ADC_CFG1_ADLSMP_MASK |ADC_CFG1_MODE(1)|ADC_CFG1_ADICLK(0); //ADC_CFG1_ADIV(en 0) para no promediar ADC_CFG1_MODE(en 1) para 12 bits
+	//Configuración ADC
+	ADC0->CFG1 = ADC_CFG1_ADIV(0) | ADC_CFG1_ADLSMP_MASK |
+	ADC_CFG1_MODE(1) | ADC_CFG1_ADICLK(0);
 	ADC0->CFG2 = 0;
 	ADC0->SC2 = 0;
 	ADC0->SC3 = ADC_SC3_AVGE_MASK | ADC_SC3_AVGS(3);
 
-	//	Inicialización del DAC0
-	SIM->SCGC2 = 0x1000;
+	//Configuración DAC
 	DAC0->C0 = 0xC0;
 	DAC0->DAT[0].DATL = 0;
 	DAC0->DAT[0].DATH = 0;
 
 	for (;;) {
+		//Entrada del ADC
 		AudIn = ADC_result(ADC_0);
 		Salida = AudIn;
 
+		//Lectura de estado de push button
+		input_value_SW3 = PushButton_read(PB_SW3);
+		input_value_SW2 = PushButton_read(PB_SW2);
+
+		//Validación de cual botón se tiene presionado ----------------------------------------------
+		if (FALSE == input_value_SW3) {		//SW3, aumenta volumen
+			delay(DELAY_ARG);				//Debouncer
+			input_value_SW2 = PushButton_read(PB_SW2); //Valida si hay mas de un botón presionado
+			if (FALSE == input_value_SW2) { //SW2
+				delay(DELAY_ARG);			//Debouncer
+				//si ambos sw están presionados, revisa el estado de la convolucion
+
+			} else { //Si solo un sw está presionado
+
+			}
+		}
+
+		//Validación de cual botón se tiene presionado ----------------------------------------------
+		if (FALSE == input_value_SW2) { 	//SW2, baja volumen
+			delay(DELAY_ARG);				//Debouncer
+			input_value_SW3 = PushButton_read(PB_SW3);//Valida si hay mas de un botón presionado
+			if (FALSE == input_value_SW3) {	//SW3
+				delay(DELAY_ARG);			//Debouncer
+				//si ambos sw están presionados, revisa el estado de la convolucion
+
+			} else { //Si solo un sw está presionado
+
+			}
+		}
+
+		//Salida al DAC
 		SalidaL = (uint16_t) Salida & 0xFF;
 		SalidaH = (uint16_t) ((Salida >> 8) & 0x0F);
-
 		DAC0->DAT[0].DATL = SalidaL;
 		DAC0->DAT[0].DATH = SalidaH;
 
@@ -96,10 +133,9 @@ void main(void) {
 
 }
 
-
 uint16_t ADC_result(uint8_t ADC) {
 	uint16_t adc_result;
-	ADC0->SC1[0] = ADC_SC1_ADCH(17);//PTE24 tarjeta, ADC0SE17 mapeado en 10001
+	ADC0->SC1[0] = ADC_SC1_ADCH(17); //PTE24 tarjeta, ADC0SE17 mapeado en 10001
 	while ((ADC0->SC1[0] & ADC_SC1_COCO_MASK) == 0)
 		;
 	adc_result = ADC0->R[0];
